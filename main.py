437302cd341ea8ea2c1a4e8ff30be9ccfdcb8744
kingkendrew06 <shen.kendrew@gmail.com>
2022-08-04 00:58:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# copyright Kendrew Shen 2022
+
+print("Welcome to the Green Mountains")
+print("press enter to continue")
+input()
+NAME = input("What is your name?:")
+input("Press any key to continue")
+OPTION = input()
+print("Choose 1 of the following options")
+print("A=Start your adventure")
+OPTION = input()
+if OPTION == "A":
+  print("A")
